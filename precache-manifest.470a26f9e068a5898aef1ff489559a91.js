a205b61ed3c3b0866b78402ec5e682afb9e0c541
McCormack <admccormack@deloitte.co.uk>
2020-01-29 14:28:47 +0000

    Updates

---
+++ precache-manifest.470a26f9e068a5898aef1ff489559a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bd94a4207a36865e1b20619f943243",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c0a0fc782e4120f1e67e",
+    "url": "/monsters-rolodex/static/css/main.dd5b120b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0a0fc782e4120f1e67e",
+    "url": "/monsters-rolodex/static/js/main.3d1af4ab.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
